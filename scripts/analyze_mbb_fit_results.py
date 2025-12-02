d954db69fc6cb6a041ff98a61a2af01341c2801e
JobVorster <56391535+JobVorster@users.noreply.github.com>
2025-12-02 16:05:58 +0200

    Blackbody fitting

---
+++ scripts/analyze_mbb_fit_results.py
@@ -0,0 +1,338 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import os
+from ifu_analysis.jdspextract import read_aperture_ini
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+
+pd.set_option('display.max_columns', None)
+
+source_name = 'L1448MM1'
+extinction_correction = True
+
+
+#Define aperture filename (for aperture names and radii).
+aperture_filename = '/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/input-files/aperture-ini-%s.txt'%(source_name)
+
+if os.path.isfile(aperture_filename):
+	aper_names,aper_sizes,coord_list = read_aperture_ini(aperture_filename)
+	skycoord_arr = [SkyCoord(ra,dec,frame='icrs') for (ra,dec) in coord_list]
+	skycoord_sep_arr = [skycoord_arr[0].separation(skycoord_arr[i]).to(u.arcsec) for i in range(len(coord_list))]
+
+if extinction_correction:
+	input_foldername = '/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/output-files/L1448MM1_post_processing/spectral_extraction/spectra_bb/Extinction_Corrected/'
+else:
+	input_foldername ='/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/output-files/L1448MM1_post_processing/spectral_extraction/spectra_bb/'
+
+results_filename = input_foldername + 'fit_results.csv'
+
+chi2_red_ceiling = 500
+
+df_mbbresults = pd.read_csv(results_filename)
+
+do_boxplot = False
+if do_boxplot:
+	model_arr = ['2BB','2BB-SP','2BB-SO','2BB-SPO','2BB-SPO-WW','2BB-SPO-WH','2BB-SPO-WWH']
+	for psfsub in [True, False]:
+		#Plots of goodness of fit statistics per model.
+		chi2_2D_arr = []
+		for model in model_arr:
+			df_mbbresults = pd.read_csv(results_filename)
+
+			#PSFSub filtering
+			inds_psfsub = np.where(df_mbbresults['PSF Subtracted (T/F)'].values==psfsub)[0]
+			df_mbbresults = df_mbbresults.iloc[inds_psfsub]
+
+
+			#Remove values where the fit did not work.
+			inds_badfits = np.where(df_mbbresults['Fit Successful (T/F)'].values!=False)[0]
+			df_mbbresults = df_mbbresults.iloc[inds_badfits]
+
+			#Model filtering
+			inds_model = np.where(df_mbbresults['Model'].values==model)[0]
+			df_mbbresults = df_mbbresults.iloc[inds_model]
+
+			chi2_2D_arr.append(df_mbbresults['chi2_reduced'].values)
+
+		plt.figure(figsize = (10,4))
+		plt.boxplot(chi2_2D_arr,tick_labels=model_arr)
+		plt.gca().tick_params(axis='x', rotation=45)
+
+		plt.ylabel('chi2_reduced')
+		plt.suptitle('Source: %s, PSFSUB: %s, Av Corrected: %s'%(source_name,psfsub, extinction_correction)+'\n Codes Legend- SO: Olivine, SP: Pyroxene, WW: 15 K H2O, WH: 150 K H2O')
+		plt.show()
+
+psfsub = True 
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+ORANGE = '\033[38;5;208m'
+RESET = '\033[0m'
+BOLD = '\033[1m'
+
+COL_WIDTH = 12
+SIG_FIGS = 3
+
+model_params = {
+    'BB': 2,              # temp, scaling
+    'BB-SP': 3,           # temp, scaling, pyroxene_scaling
+    'BB-SO': 3,           # temp, scaling, olivine_scaling
+    'BB-SPO': 4,          # temp, scaling, pyroxene_scaling, olivine_scaling
+    'BB-SPO-WW': 5,       # BB-SPO + h2o15K_scaling (note: "WW" seems to be just one water component)
+    'BB-SPO-WH': 5,       # BB-SPO + h2o150K_scaling
+    'BB-SPO-WWH': 6,      # BB-SPO + h2o15K_scaling + h2o150K_scaling
+    '2BB': 4,             # temp1, scaling1, temp2, scaling2
+    '2BB-SP': 5,          # 2BB + pyroxene_scaling
+    '2BB-SO': 5,          # 2BB + olivine_scaling
+    '2BB-SPO': 6,         # 2BB + pyroxene_scaling + olivine_scaling
+    '2BB-SPO-WW': 7,      # 2BB-SPO + h2o15K_scaling
+    '2BB-SPO-WH': 7,      # 2BB-SPO + h2o150K_scaling
+    '2BB-SPO-WWH': 8      # 2BB-SPO + h2o15K_scaling + h2o150K_scaling
+}
+
+
+for aperture in aper_names:
+	df_mbbresults = pd.read_csv(results_filename)
+	#Remove values where the fit did not work.
+	inds_badfits = np.where(df_mbbresults['Fit Successful (T/F)'].values!=False)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_badfits]
+
+	#Values without psf subtraction.
+	inds_psfsub = np.where(df_mbbresults['PSF Subtracted (T/F)'].values==psfsub)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_psfsub]
+
+	inds_aperture = np.where(df_mbbresults['Aperture'].values==aperture)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_aperture]
+	print('#'*30)
+	print('#'*9 + ' '*5 + aperture + ' '*5 + '#'*9 )
+	print('#'*30)
+
+	sep_str = ' '*5
+
+	cols = ['Model', 'BB1T','BB1S','BB2T','BB2S','Sil Oliv','Sil Pyro','H2O 15K','H2O 150K','BIC','chi2_reduced']
+	print_str = ''
+	for col in cols[:-1]:
+		print_str += f'{col:<{COL_WIDTH}}'
+	print_str += f'{cols[-1]:<{COL_WIDTH}}'
+	print(print_str)
+
+	sort_by = 'chi2_reduced'
+
+	chi2_vals = df_mbbresults[sort_by].values
+	if len(chi2_vals) > 0:
+		sorted_indices = sorted(range(len(chi2_vals)), key=lambda i: chi2_vals[i])
+		lowest_3 = set(sorted_indices[:3])
+		colors = {sorted_indices[0]: GREEN, sorted_indices[1]: YELLOW, sorted_indices[2]: ORANGE}
+
+
+	for i in range(len(df_mbbresults['Aperture'].values)):
+		print_vals = [df_mbbresults[x].values[i] for x in cols]
+		print_str = ''
+
+		for val,col in zip(print_vals[:-1],cols[:-1]):
+			if col in ['BB1T','BB2T', 'chi2_reduced','BIC']:
+				FORMATTER = 'f'
+				SIG_FIGS = 0
+			else:
+				FORMATTER = 'E'
+				SIG_FIGS = 2
+
+			if type(val) == str:
+				print_str += f'{val:<{COL_WIDTH}}'
+			else:
+				print_str += f'{val:<{COL_WIDTH}.{SIG_FIGS}{FORMATTER}}'
+		val = print_vals[-1]
+
+		#Want to see the Chi2 as a integer.
+		FORMATTER = 'f'
+		SIG_FIGS = 2
+		formatted = f'{val:<{COL_WIDTH}}' if isinstance(val, str) else f'{val:<{COL_WIDTH}.{SIG_FIGS}{FORMATTER}}'
+		print_str += formatted
+		
+		# Add color to entire row if this row is in top 3 lowest chi2
+		if i in lowest_3:
+			print_str =  BOLD + colors[i] + print_str + RESET
+		
+		print(print_str)
+
+
+	#print(df_mbbresults[['Model','BB1T','BB1S','BB2T','BB2S','chi2_reduced']])
+
+#Plot best fit T1 and S1
+
+aperture_plots = {}
+
+for aperture in aper_names:
+	df_mbbresults = pd.read_csv(results_filename)
+	#Remove values where the fit did not work.
+	inds_badfits = np.where(df_mbbresults['Fit Successful (T/F)'].values!=False)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_badfits]
+
+	#Values without psf subtraction.
+	inds_psfsub = np.where(df_mbbresults['PSF Subtracted (T/F)'].values==psfsub)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_psfsub]
+
+	inds_aperture = np.where(df_mbbresults['Aperture'].values==aperture)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_aperture]
+	if len(df_mbbresults['Aperture']) > 0:
+		sort_by = 'chi2_reduced'
+		chi2_vals = df_mbbresults[sort_by].values
+		if len(chi2_vals) > 0:
+			sorted_indices = sorted(range(len(chi2_vals)), key=lambda i: chi2_vals[i])
+			lowest_3 = set(sorted_indices[:3])
+		ind_select = sorted_indices[0]
+
+		aper_ind = np.where(aper_names==aperture)[0][0]
+
+		angular_sep = skycoord_sep_arr[aper_ind].value #arcsec
+
+		aperture_plots['%s:Angular_Sep'%(aperture)] = angular_sep
+		aperture_plots['%s:Temp1'%(aperture)] = df_mbbresults['BB1T'].values[ind_select]
+		aperture_plots['%s:Scale1'%(aperture)] = df_mbbresults['BB1S'].values[ind_select]
+
+
+plt.figure(figsize = (10,4))
+for ap_series in ['A','B','C']:
+	sep_arr = []
+	sep_cols = []
+	for col in aperture_plots.keys():
+		if ('Angular_Sep' in col) and (col[0] == ap_series):
+			sep_cols.append(col)
+
+	t_cols = []
+	for col in aperture_plots.keys():
+		if ('Temp1' in col) and (col[0] == ap_series):
+			t_cols.append(col)
+
+	scale_cols = []
+	for col in aperture_plots.keys():
+		if ('Scale1' in col) and (col[0] == ap_series):
+			scale_cols.append(col)
+
+	for col in sep_cols:
+		sep_arr.append(aperture_plots[col])
+
+	t_arr = []
+	for col in t_cols:
+		t_arr.append(aperture_plots[col])
+
+	scale_arr = []
+	for col in scale_cols:
+		scale_arr.append(aperture_plots[col])
+
+	
+	plt.subplot(121)
+	plt.scatter(sep_arr,t_arr,label = ap_series)
+	plt.xlabel('Angular Separation (arcsec)')
+	plt.ylabel('Hot Blackbody Temperature (K)')
+	plt.subplot(122)
+	plt.scatter(sep_arr,scale_arr,label = ap_series)
+	plt.xlabel('Angular Separation (arcsec)')
+	plt.ylabel('Hot Blackbody Scale (aperture solid angle)')
+plt.subplot(121)
+plt.legend()
+plt.subplot(122)
+plt.legend()
+plt.show()
+
+
+
+
+
+
+model_arr = ['2BB-SPO','2BB-SPO-WWH']
+#Plots of quantities' distance dependence.
+for model in model_arr:
+	df_mbbresults = pd.read_csv(results_filename)
+	#Index(['Source Name', 'Aperture', 'PSF Subtracted (T/F)', 'Model',
+	#       'Fit Successful (T/F)', 'BB1T', 'lsqunc BB1T', 'BB1S', 'lsqunc BB1S',
+	#       'BB2T', 'lsqunc BB2T', 'BB2S', 'lsqunc BB2S', 'Sil Oliv',
+	#       'lsqunc Sil Oilv', 'Sil Pyro', 'lsqunc Sil Pyro', 'H2O 15K',
+	#       'lsqunc H2O 15K', 'H2O 150K', 'lsqunc H2O 150K', 'chi2', 'chi2_reduced',
+	#       'AIC', 'BIC']
+
+
+	#Remove values where the fit did not work.
+	inds_badfits = np.where(df_mbbresults['Fit Successful (T/F)'].values!=False)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_badfits]
+
+	#Remove fits with too high chi_red:
+	inds_chi2red = np.where(df_mbbresults['chi2_reduced'].values< chi2_red_ceiling)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_chi2red]
+
+	#Remove the non-psfsubbed indices.
+	psfsub = True
+	inds_psfsub = np.where(df_mbbresults['PSF Subtracted (T/F)'].values==psfsub)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_psfsub]
+
+	inds_model = np.where(df_mbbresults['Model'].values==model)[0]
+	df_mbbresults = df_mbbresults.iloc[inds_model]
+
+	#Lets only look at aperture A2
+	#aperture = 'A3'
+	#inds_aperture = np.where(df_mbbresults['Aperture'].values==aperture)[0]
+	#df_mbbresults = df_mbbresults.iloc[inds_aperture]
+
+	col_arr =['Aperture','BB1T','Sil Oliv','BB2T','Sil Pyro']
+
+	plot_results = {}
+	for c in ['A','B','C']:
+		plot_results['%s:Angular_Sep'%(c)] = []
+		plot_results['%s:%s'%(c,col_arr[1])] = []
+		plot_results['%s:%s'%(c,col_arr[2])] = []
+		plot_results['%s:%s'%(c,col_arr[3])] = []
+		plot_results['%s:%s'%(c,col_arr[4])] = []
+		
+	dfaperture, dfbb1t, dfbb1s, dfbb2t, dfbb2s=[df_mbbresults[col].values for col in col_arr]
+	for aperture, bb1t, bb1s, bb2t, bb2s in zip(dfaperture, dfbb1t, dfbb1s, dfbb2t, dfbb2s) :
+		if aperture =='PS':
+			continue
+
+		if (bb1t > 1e4) or (bb2t > 1e4):
+			continue
+
+		c = aperture[0]
+		aper_ind = np.where(aper_names==aperture)[0][0]
+
+		angular_sep = skycoord_sep_arr[aper_ind].value #arcsec
+		
+		plot_results['%s:Angular_Sep'%(c)].append(angular_sep)
+		plot_results['%s:%s'%(c,col_arr[1])].append(bb1t)
+		plot_results['%s:%s'%(c,col_arr[2])].append(bb1s)
+		plot_results['%s:%s'%(c,col_arr[3])].append(bb2t)
+		plot_results['%s:%s'%(c,col_arr[4])].append(bb2s)
+
+	for c in ['A','B','C']:
+		plotcol_arr = ['%s:Angular_Sep'%(c),'%s:%s'%(c,col_arr[1]),'%s:%s'%(c,col_arr[2]),'%s:%s'%(c,col_arr[3]),'%s:%s'%(c,col_arr[4])]
+		angular_sep_arr,bb1t_arr,bb1s_arr,bb2t_arr,bb2s_arr = [plot_results[col] for col in plotcol_arr]
+
+		plt.subplot(221)
+		plt.scatter(angular_sep_arr,bb1t_arr,label='%s'%(c))
+		#plt.ylim(500,1500)
+		plt.legend()
+		plt.xlabel('Angular Separation (arcsec)')
+		plt.ylabel('%s'%(col_arr[1]))
+		plt.subplot(222)
+		plt.scatter(angular_sep_arr,bb1s_arr,label='%s'%(c))
+		plt.legend()
+		plt.xlabel('Angular Separation (arcsec)')
+		plt.ylabel('%s'%(col_arr[2]))
+		plt.subplot(223)
+		plt.scatter(angular_sep_arr,bb2t_arr,label='%s'%(c))
+		plt.legend()
+		plt.xlabel('Angular Separation (arcsec)')
+		#plt.ylim(200,500)
+		plt.ylabel('%s'%(col_arr[3]))
+		plt.subplot(224)
+		plt.scatter(angular_sep_arr,bb2s_arr,label='%s'%(c))
+		plt.legend()
+		plt.xlabel('Angular Separation (arcsec)')
+		plt.ylabel('%s'%(col_arr[4]))
+	plt.suptitle('Source: %s, PSFSUB: %s, Model: %s'%(source_name,psfsub,model)+'\n SO: Olivine, SP: Pyroxene, WW: 15 K H2O, WH: 150 K H2O')
+	plt.show()
+
+
+
+
+	
+print(df_mbbresults[['Aperture','PSF Subtracted (T/F)', 'Model','BB1T','BB1S','BB2T','BB2S','chi2_reduced']])
--- scripts/blackbody_fitting.py
+++ scripts/blackbody_fitting.py
@@ -1,4 +1,5 @@
 from ifu_analysis.jdspextract import load_spectra,merge_subcubes,read_aperture_ini
+from ifu_analysis.jdutils import define_circular_aperture
 from ifu_analysis.jdfitting import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,166 @@ from scipy.optimize import curve_fit
 from pybaselines import Baseline
 import corner
 import emcee
+from astropy.wcs import WCS
+from astropy.io import fits
+import pandas as pd
+
+def parse_fit_results(source_name,aperture,which_data,fitoption,popt,pcov,goodness_of_fit,fit_successful = False):
+	
+	#fitoptions = ['BB','BB-SP','BB-SO','BB-SPO',
+	#		  'BB-SPO-WW', 'BB-SPO-WH','BB-SPO-WWH',
+	#		  '2BB','2BB-SP','2BB-SO','2BB-SPO',
+	#		  '2BB-SPO-WW','2BB-SPO-WH','2BB-SPO-WWH']
+
+
+	col_arr = [
+	'Source Name', #Ind 0
+	'Aperture', #Ind 1
+	'PSF Subtracted (T/F)', #Ind 2
+	'Model', #Ind 3
+	'Fit Successful (T/F)', #Ind 4
+	'BB1T', #Ind 5
+	'lsqunc BB1T', #Ind 6
+	'BB1S', #Ind 7
+	'lsqunc BB1S', #Ind 8
+	'BB2T', #Ind 9
+	'lsqunc BB2T', #Ind 10
+	'BB2S', #Ind 11
+	'lsqunc BB2S', #Ind 12
+	'Sil Oliv', #Ind 13
+	'lsqunc Sil Oilv', #Ind 14
+	'Sil Pyro', #Ind 15
+	'lsqunc Sil Pyro', #Ind 16
+	'H2O 15K', #Ind 17
+	'lsqunc H2O 15K', #Ind 18
+	'H2O 150K', #Ind 19
+	'lsqunc H2O 150K', #Ind 20
+	'chi2', #Ind 21
+	'chi2_reduced', #Ind 22 
+	'AIC', #Ind 23
+	'BIC' #Ind 24
+	]
+
+	fit_results = ['N/A']*len(col_arr)
+
+	#Metadata
+	fit_results[0] = source_name
+	fit_results[1] = aperture
+	if which_data == 'PSFSUB':
+		fit_results[2] = True
+	else:
+		fit_results[2] = False
+	fit_results[3] = fitoption
+
+	if not fit_successful:
+		fit_results[4] = False
+		return np.array(fit_results,dtype=str)
+	else:
+
+		#Then the fit worked.
+		fit_results[4] = True
+
+		#Turn the covariance matrix into the uncertainties --> This assumes a zero covariance.
+		#We will follow up the chosen models with MCMC simulations in any way to get true uncertainties.
+		lsqunc = np.sqrt(np.diag(pcov)) 
+
+		#We can save the goodness of fit measures already:
+		AIC, BIC,chi2,chi2_red = goodness_of_fit
+		fit_results[21] = chi2
+		fit_results[22] = chi2_red
+		fit_results[23] = AIC
+		fit_results[24] = BIC
+
+		#Split the model code.
+		#If it has length 1, it is either 1 BB or 2 BB.
+		descr = fitoption.split('-')
+		
+		#This procedure is the careful mapping of popt indices for each model type. 
+		#This function needs to be rewritten if the model types are changed.
+		#Is also extremely error prone if indices are mistakenly used.
+		#Probably this would work better with a dictionary or something.
+
+		#First, there will always be at least 1 BB.
+		fit_results[5] = popt[0]
+		fit_results[6] = lsqunc[0]
+		fit_results[7] = popt[1]
+		fit_results[8] = lsqunc[1]
+
+		#If there are two blackbodies.
+		if descr[0] == '2BB':
+			fit_results[9] = popt[2]
+			fit_results[10] = lsqunc[2]
+			fit_results[11] = popt[3]
+			fit_results[12] = lsqunc[3]
+
+		if len(descr) == 1:
+			#If there are no other model components, just return the results.
+			return np.array(fit_results,dtype=str)
+		else:
+			#The second is always sillicates (there are no options with water and no sillicates).
+			#So it is either Olivine only (code SO), Pyroxene only (code SP) or both (code SPO).
+
+			#Now the popt index depends on whether its one or two blackbodies.
+			if descr[1] == 'SO':
+				if descr[0] == 'BB':
+					fit_results[13] = popt[2]
+					fit_results[14] = lsqunc[2]
+				else:
+					fit_results[13] = popt[4]
+					fit_results[14] = lsqunc[4]
+			#At least the logic is exactly the same for the pyroxene case.
+			elif descr[1] =='SP':
+				if descr[0] == 'BB':
+					fit_results[15] = popt[2]
+					fit_results[16] = lsqunc[2]
+				else:
+					fit_results[15] = popt[4]
+					fit_results[16] = lsqunc[4]
+			elif descr[1] =='SPO':
+				if descr[0] == 'BB':
+					fit_results[13] = popt[2]
+					fit_results[14] = lsqunc[2]
+					fit_results[15] = popt[3]
+					fit_results[16] = lsqunc[3]
+				else:
+					fit_results[13] = popt[4]
+					fit_results[14] = lsqunc[4]
+					fit_results[15] = popt[5]
+					fit_results[16] = lsqunc[5]
+			else:
+				raise ValueError('Something went wrong in the model codes when parsing the fit results. Exiting...')
+
+
+			#Gross. Now it will only get worse. Time to check if there is water.
+			#This should really be rewritten. But I am just trying to get it working now while my 7 month old is sleeping.
+
+			if len(descr) ==2:
+				return np.array(fit_results,dtype=str)
+			else:
+				#The third is always water, and the second is always sillicates.
+				#The water can be warm (code WW), hot (code WH) or both (code WWH).
+				#The second is always sillicates
+				#So it is either Olivine only (code SO), Pyroxene only (code SP) or both (code SPO).
+				#Then there is whether there are 2 BBs or not --> Twelve options in all. 
+
+				#BUT, we can take advantage of that the sillicates and BBs are already set, and that we know the relative water postions.
+				if descr[2] == 'WW':
+					fit_results[17] = popt[-1]
+					fit_results[18] = lsqunc[-1]
+				elif descr[2] == 'WH':
+					fit_results[19] = popt[-1]
+					fit_results[20] = lsqunc[-1]
+				else:
+					fit_results[17] = popt[-2]
+					fit_results[18] = lsqunc[-2]
+					fit_results[19] = popt[-1]
+					fit_results[20] = lsqunc[-1]
+				#BOOM! Done.
+				#TODO: Write code that does not look like a 12-year old wrote it.
+
+				return np.array(fit_results,dtype=str)
+
+
 
 def plot_fit(um, flux, flux_unc,fitoption,fit_models_dict,popt,pcov, fit_wavelengths,N_sigma_clip, source_name, aperture,which_data,ylim=[1e-22,5e-17],AIC =None,BIC = None,cont_mask=None,saveto=None):
 	aanda_col_width = 3.46457 #inches
@@ -66,11 +227,20 @@ def plot_fit(um, flux, flux_unc,fitoption,fit_models_dict,popt,pcov, fit_wavelen
 	else:
 		plt.show()
 
+def extinction(wave1,av): 
+  extinc=np.interp(wave1,wave,Av)*av
+  return (extinc)
+				    
+
 #Here I need to put all the fitting subcomponents plotting.
 #WRITE THIS FUNCTION.
 #This function also needs some customization by the user.
 def plot_subcomponents(um, flux, flux_unc,fitoption,fit_models_dict,popt,pcov):
 	model = fit_models_dict[fitoption]
+
+	#need some sort of algorithm to plot fitting parameters.
+
+
 	if fitoption == 'BB+Sillicate':
 		T1, S1, T2, S2, Sil_scaling = popt
 		#BB1
@@ -104,8 +274,18 @@ fn_sillicates = "adwin_silicates.csv"
 
 
 source_name = 'L1448MM1'
-
+do_sigma_clip = True
+do_extinction_correction = True
 N_sigma_clip = 3
+
+
+#Wavelength to fit.
+fit_wavelengths = [[4.92,5.6],[5.61,6],[6.5,6.56],[7.3,7.605],[7.75,14.5],[16,27.5]]
+
+#Data subgroups to iterate over.
+which_datas = ['NOSUB','PSFSUB']
+
+
 
 #Which model options to fit.
 fitoptions = ['BB','BB-SP','BB-SO','BB-SPO',
@@ -123,18 +303,18 @@ for fitoption, fitmodel in zip(fitoptions,fitmodels):
 
 #Initial guesses for the different fit options.
 
-T1_guess = 700
-S1_guess = 1e-9
-T2_guess = 50
-S2_guess = 1e-3
-Sil_guess = 1e-2
-Wat_guess = 1e-2
+T1_guess = 800
+S1_guess = 1e-5
+T2_guess = 300
+S2_guess = 1e-5
+Sil_guess = 1e-1
+Wat_guess = 1e-1
 
 
 #This is a lazy little way to code the initial conditions.
 #Its based on the names of the models.
 #The name always goes blackbody type - Sillicates - Water
-#Based on the number of blackbodies there is a guess.
+#Based on the number of blackbodies there is a p0 guess.
 #Then based on the length of the sillicate and water, there is either one or two guesses added.
 p0_dict = {}
 for fitoption in fitoptions:
@@ -152,11 +332,7 @@ for fitoption in fitoptions:
 		ValueError('There is some sort of issue with the way the models are named. Either change the model name, or the initial guess initialization.')
 	p0_dict[fitoption] = p0
 
-#Wavelength to fit.
-fit_wavelengths = [[4.92,5.6],[5.61,6],[6.5,6.56],[7.3,7.605],[7.75,14.5],[16,27.5]]
 
-#Data subgroups to iterate over.
-which_datas = ['NOSUB','PSFSUB']
 
 #Define aperture filename (for aperture names and radii).
 aperture_filename = '/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/input-files/aperture-ini-%s.txt'%(source_name)
@@ -164,15 +340,86 @@ aperture_filename = '/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/input-file
 #Filename where to save results (with subfiles for each which_data and fitoption precreated).
 output_foldername ='/home/vorsteja/Documents/JOYS/JDust/ifu_analysis/output-files/L1448MM1_post_processing/spectral_extraction/spectra_bb/'
 
+
+#Filename of extinction corrections.
+if do_extinction_correction:
+	fn_extinction = '/home/vorsteja/Documents/JOYS/Collaborator_Scripts/Gabriella_L1448MM/Av_map_fit_SNR30_KP5.fits'
+
+#Columns for saving the fit results.
+col_arr = [
+	'Source Name', #Ind 0
+	'Aperture', #Ind 1
+	'PSF Subtracted (T/F)', #Ind 2
+	'Model', #Ind 3
+	'Fit Successful (T/F)', #Ind 4
+	'BB1T', #Ind 5
+	'lsqunc BB1T', #Ind 6
+	'BB1S', #Ind 7
+	'lsqunc BB1S', #Ind 8
+	'BB2T', #Ind 9
+	'lsqunc BB2T', #Ind 10
+	'BB2S', #Ind 11
+	'lsqunc BB2S', #Ind 12
+	'Sil Oliv', #Ind 13
+	'lsqunc Sil Oilv', #Ind 14
+	'Sil Pyro', #Ind 15
+	'lsqunc Sil Pyro', #Ind 16
+	'H2O 15K', #Ind 17
+	'lsqunc H2O 15K', #Ind 18
+	'H2O 150K', #Ind 19
+	'lsqunc H2O 150K', #Ind 20
+	'chi2', #Ind 21
+	'chi2_reduced', #Ind 22 
+	'AIC', #Ind 23
+	'BIC' #Ind 24
+	]
+
+def list_to_str(list1,sep=','):
+	st = ''
+	for i,item in enumerate(list1):
+		if i != len(list1)-1:
+			st += item +sep
+		else:
+			st += item +'\n'
+	return st
+
+results_filename = output_foldername + 'fit_results.csv'
+if os.path.exists(results_filename):
+  os.remove(results_filename)
+
+f = open(results_filename,'w+')
+f.write(list_to_str(col_arr))
+f.close()
+
+extinction_arr = []
+
+
 if os.path.isfile(aperture_filename):
 	aper_names,aper_sizes,coord_list = read_aperture_ini(aperture_filename)
+	for aper_name, aper_size, coords in zip(aper_names,aper_sizes,coord_list):
+		if do_extinction_correction:
+			hdu_ext = fits.open(fn_extinction)
+			wcs = WCS(hdu_ext[0].header)
+			ext = hdu_ext[0].data 
+
+
+
+			shp = np.shape(ext)
+			aper = define_circular_aperture(coords[0],coords[1],aper_size) #Skyaperture 
+			mask_par = aper.to_pixel(wcs)
+			aper_mask = np.mod(np.array(mask_par.to_mask(method='center').to_image(shp),dtype=int),2)
+			inds = np.where(aper_mask ==1)
+			extinction_arr.append(np.nanmean(ext[inds]))
+if do_extinction_correction:
+	print(extinction_arr)	
+
 
 for fitoption in fitoptions:
 
 	descr = fitoption.split('-')
 	#Only do 2BB fits.
-	if descr[0] == 'BB':
-		continue	
+	#if descr[0] == 'BB':
+	#	continue	
 
 	for which_data in which_datas:
 
@@ -180,10 +427,10 @@ for fitoption in fitoptions:
 		if which_data == 'NOSUB':
 			continue
 
-		for aperture, ap_rad in zip(aper_names,aper_sizes):
+		for i_ap, (aperture, ap_rad) in enumerate(zip(aper_names,aper_sizes)):
 
-			if ('B' in aperture) or ('C' in aperture):
-				continue
+			#if ('A' in aperture) or ('C' in aperture):
+			#	continue
 
 			print('Doing Source: %s, Fit option: %s, Aperture: %s, Model: %s'%(source_name,which_data,aperture,fitoption))
 
@@ -204,23 +451,51 @@ for fitoption in fitoptions:
 
 			#Sigma clipping.
 			#TODO: Do the sigma clipping at the stitching to have correct uncertainties.
-			
-			inds_base = np.where(flux_base > N_sigma_clip*unc_base)
-			inds_psfmodel = np.where(flux_psfmodel > N_sigma_clip*unc_psfmodel)
-			inds_psfsub = np.where(flux_psfsub > N_sigma_clip*unc_psfsub)
+			if do_sigma_clip:
+				inds_base = np.where(flux_base > N_sigma_clip*unc_base)
+				inds_psfmodel = np.where(flux_psfmodel > N_sigma_clip*unc_psfmodel)
+				inds_psfsub = np.where(flux_psfsub > N_sigma_clip*unc_psfsub)
 
-			#Flag sigma_clipped measurements.
-			if which_data =='NOSUB':
-				u_use = um_base[inds_base]
-				f_use = flux_base[inds_base]
-				unc_use = unc_base[inds_base]
-			elif which_data =='PSFSUB':
-				u_use = um_psfsub[inds_psfsub]
-				f_use = flux_psfsub[inds_psfsub]
-				unc_use = unc_psfsub[inds_psfsub]
+				#Flag sigma_clipped measurements.
+				if which_data =='NOSUB':
+					u_use = um_base[inds_base]
+					f_use = flux_base[inds_base]
+					unc_use = unc_base[inds_base]
+				elif which_data =='PSFSUB':
+					u_use = um_psfsub[inds_psfsub]
+					f_use = flux_psfsub[inds_psfsub]
+					unc_use = unc_psfsub[inds_psfsub]
+				else:
+					raise ValueError('Please specify which data would be used (BASE/PSFSUB).')
+					exit()
 			else:
-				raise ValueError('Please specify which data would be used (BASE/PSFSUB).')
-				exit()
+				if which_data =='NOSUB':
+					u_use = um_base
+					f_use = flux_base
+					unc_use = unc_base
+				elif which_data =='PSFSUB':
+					u_use = um_psfsub
+					f_use = flux_psfsub
+					unc_use = unc_psfsub
+				else:
+					raise ValueError('Please specify which data would be used (BASE/PSFSUB).')
+					exit()
+
+			if do_extinction_correction:
+				ext_app = extinction_arr[i_ap]
+
+				file = "/home/vorsteja/Documents/JOYS/Collaborator_Scripts/Sam_Extinction/KP5_benchmark_RNAAS.csv"  # OpTool version
+
+				df=pd.read_csv(file)
+
+				wave=df['wavelength']
+				Av=df['Av']
+				ext_um = extinction(u_use,ext_app)
+				f_use *= np.exp(ext_um/1.086)
+				unc_use	*= np.exp(ext_um/1.086)
+
+			
+
 
 			#Convert from Jy to W cm-2
 			f_rad = convert_flux_wl(u_use,f_use)
@@ -293,19 +568,31 @@ for fitoption in fitoptions:
 				model = fit_models_dict[fitoption]
 
 				p0 = p0_dict[fitoption]
+
+				popt,pcov = fit_model(fit_um, fit_flux, fit_unc,model,p0)
+
 				try:
 					popt,pcov = fit_model(fit_um, fit_flux, fit_unc,model,p0)
-					AIC, BIC = calculate_information_criterion(fit_um, fit_flux, model, popt)
+					goodness_of_fit = calculate_goodness_of_fit(fit_um, fit_flux,fit_unc, model, popt,pcov)
+					AIC, BIC,chi2,chi2_red = goodness_of_fit
+					print('Fit successfull (reduced chi2: %.2f) with popt: %s'%(chi2_red,str(['%.2E'%(x) for x in popt])))
+					fit_results = parse_fit_results(source_name,aperture,which_data,fitoption,popt,pcov,goodness_of_fit,fit_successful = True)
 
-					plot_fit(u_use, f_rad, unc_rad,fitoption,fit_models_dict,popt,pcov, fit_wavelengths,N_sigma_clip, source_name, aperture,which_data,AIC=AIC,BIC=BIC,cont_mask=cont_mask,saveto=None)
+					f = open(results_filename,'a')
+					f.write(list_to_str(fit_results))
+					f.close()
+
+					saveto = output_foldername + '%s_%s_%s_%s_fit.png'%(source_name,aperture,which_data,fitoption)
+					plot_fit(u_use, f_rad, unc_rad,fitoption,fit_models_dict,popt,pcov, fit_wavelengths,N_sigma_clip, source_name, aperture,which_data,AIC=AIC,BIC=BIC,cont_mask=cont_mask,saveto=saveto)
 				except:
+					fit_results = parse_fit_results(source_name,aperture,which_data,fitoption,popt=[],pcov=[],goodness_of_fit=[],fit_successful = False)
+					f = open(results_filename,'a')
+					f.write(list_to_str(fit_results))
+					f.close()
 					print('No fit was obtained. Continuing to next aperture model combination...')
 					continue
 				
 
-
-
-			
 
 
 '''
